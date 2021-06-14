c1d0a85dade192583c2f3e74aa3e841110a09125
Louis Lefevre <louislefev@gmail.com>
2021-06-14 23:47:15 +0100

    Rename process.go to parse.go
    Also moved syncFiles into new system.go file.

--- internal/parse.go
+++ internal/parse.go
@@ -118,24 +118,3 @@ func visit(files *[]string, included []string, excluded []string) filepath.WalkF
 		return nil
 	}
 }
-
-func syncFiles(dotfiles []dotfile) error {
-	for _, file := range dotfiles {
-		if file.isUpToDate() {
-			continue
-		}
-		fmt.Printf("Updating %s (%+d lines)\n", file.repoFileName, file.lineCountDiff())
-
-		if _, err := os.Stat(file.repoFilePath); os.IsNotExist(err) {
-			os.MkdirAll(filepath.Dir(file.repoFilePath), os.ModePerm)
-		} else if err != nil {
-			return err
-		}
-
-		err := ioutil.WriteFile(file.repoFilePath, file.systemFileContents, 0644)
-		if err != nil {
-			return err
-		}
-	}
-	return nil
-}
